cbb1f3bbe25a516eb04e5d6151a8ed00ef2ad720
Matix0111 <bradthompson401@gmail.com>
2021-07-17 00:59:17 -0400

    Added cleaning of pycache and some Windows support

--- mainP.py
+++ mainP.py
@@ -1,61 +1,81 @@
 import subprocess as sp
+import platform
+import atexit
+import shutil
 from program.downloadImage import dlImage as m1
 from program.userSearch import main as m2
 from program.picker import picker as m3
 from program.md5_to_post import run as m4
 from program.dlallfromusr import Program as m5
 
+OS = platform.system()
+
+@atexit.register
+def clear_pycache():
+    try:
+        shutil.rmtree('__pycache__/')
+    except FileNotFoundError:
+        pass
+    finally:
+        shutil.rmtree('program/__pycache__')
+
 def menu():
-    sp.call('clear')
-    print("#########################################")
-    print("# ################## ################## #")
-    print("# #                # #                # #")
-    print("# #    ############# #    ############# #")
-    print("# #    #             #    #             #")
-    print("# #    #             #    #             #")
-    print("# #    #	     #    #             #")
-    print("# #    #             #    #             #")
-    print("# #    #             #    #             #")
-    print("# #    ########      #    ############# #")
-    print("# #           #      #                # #")
-    print("# #    ########      #    ########    # #")
-    print("# #    #             #    #      #    # #")
-    print("# #    #             #    #      #    # #")
-    print("# #    #             #    #      #    # #")
-    print("# #    #             #    #      #    # #")
-    print("# #    #             #    #      #    # #")
-    print("# #    ############# #    ########    # #")
-    print("# #                # #                # #")
-    print("# ################## ################## #")
-    print("#########################################\n")
+    _EXIT = False
+    while not _EXIT:
+        if OS == 'Linux':
+            sp.call('clear')
+        elif OS == 'Windows':
+            sp.call('cls')
+        print("#########################################")
+        print("# ################## ################## #")
+        print("# #                # #                # #")
+        print("# #    ############# #    ############# #")
+        print("# #    #             #    #             #")
+        print("# #    #             #    #             #")
+        print("# #    #	     #    #             #")
+        print("# #    #             #    #             #")
+        print("# #    #             #    #             #")
+        print("# #    ########      #    ############# #")
+        print("# #           #      #                # #")
+        print("# #    ########      #    ########    # #")
+        print("# #    #             #    #      #    # #")
+        print("# #    #             #    #      #    # #")
+        print("# #    #             #    #      #    # #")
+        print("# #    #             #    #      #    # #")
+        print("# #    #             #    #      #    # #")
+        print("# #    ############# #    ########    # #")
+        print("# #                # #                # #")
+        print("# ################## ################## #")
+        print("#########################################\n")
 
-    print('OPTIONS:\n')
+        print('OPTIONS:\n')
 
-    print('\t Download Image            [1]')
-    print('\t User Search               [2]')
-    print('\t Explorer                  [3]')
-    print('\t MD5 To Post               [4]')
-    print('\t Download All From User    [5]')
-    print('\t Exit Program              [99]')
+        print('\t Download Image            [1]')
+        print('\t User Search               [2]')
+        print('\t Explorer                  [3]')
+        print('\t MD5 To Post               [4]')
+        print('\t Download All From User    [5]')
+        print('\t Exit Program              [99]')
 
-    option = input('\nChoice: ')
+        option = input('\nChoice: ')
 
-    if option == '1':
-        m1(RETURN=False)
-    elif option == '2':
-        m2(RETURN=False)
-    elif option == '3':
-        picOrUser = (input('Content or User? [C/u] ')).lower()
-        if picOrUser == 'u':
-            m3(MODE='usr')
-        elif picOrUser == 'c':
-            m3(MODE='img')
-    elif option == '4':
-        m4()
-    elif option == '5':
-        m5()
-    elif option == '99':
-        print('Thanks for using! Goodbye!')
+        if option == '1':
+            m1(RETURN=False)
+        elif option == '2':
+            m2(RETURN=False)
+        elif option == '3':
+            picOrUser = (input('Content or User? [C/u] ')).lower()
+            if picOrUser == 'u':
+                m3(MODE='usr')
+            elif picOrUser == 'c':
+                m3(MODE='img')
+        elif option == '4':
+            m4()
+        elif option == '5':
+            m5()
+        elif option == '99':
+            _EXIT = True
+            print('Thanks for using! Goodbye!')
 
 if __name__ == '__main__':
     menu()
